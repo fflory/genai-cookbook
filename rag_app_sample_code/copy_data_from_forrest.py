f7350f293375c0740648511c0a33b89334b7eb11
fflory <fflory>
2024-07-23 20:47:01 +0000

    copy data from forrest

---
+++ rag_app_sample_code/copy_data_from_forrest.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+!cp -r /Volumes/field_ai_examples/rag/financebench/* /Volumes/felixflory/rag_felixflory/financebench
+
+# COMMAND ----------
+
+!cp /Volumes/main/felix_flory_financebench/data/financebench_eval_set.snappy.parquet /Volumes/felixflory/rag_felixflory/financebench_eval_parquet
+
+# COMMAND ----------
+
+input_parquet_filename = '/Volumes/felixflory/rag_felixflory/financebench_eval_parquet/financebench_eval_set.snappy.parquet'
+
+# COMMAND ----------
+
+# MAGIC %run ./00_global_config
+
+# COMMAND ----------
+
+output_table = f"{UC_CATALOG}.{UC_SCHEMA}.financebench_eval_set"
+spark.read.parquet(input_parquet_filename).write.mode("overwrite").saveAsTable(output_table)
