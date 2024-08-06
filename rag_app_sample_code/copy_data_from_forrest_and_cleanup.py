6b42cd03efb0dcc7864b1a6609fcaa979295959b
fflory <fflory>
2024-08-06 22:58:29 +0000

    up

--- rag_app_sample_code/copy_data_from_forrest_and_cleanup.py
+++ rag_app_sample_code/copy_data_from_forrest_and_cleanup.py
@@ -22,3 +22,29 @@ input_parquet_filename = '/Volumes/felixflory/rag_felixflory/financebench_eval_p
 
 output_table = f"{UC_CATALOG}.{UC_SCHEMA}.financebench_eval_set"
 spark.read.parquet(input_parquet_filename).write.mode("overwrite").saveAsTable(output_table)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Cleanup
+# MAGIC
+# MAGIC manually delete serving endpoints, experiments, models, and checkpoints in UC volume, and the index
+
+# COMMAND ----------
+
+# MAGIC %run ./00_global_config
+
+# COMMAND ----------
+
+_tables_to_delete_df = spark.sql(f"show tables in {UC_CATALOG}.{UC_SCHEMA}").filter(f"tableName like '%{RAG_APP_NAME}%'")
+display(_tables_to_delete_df)
+
+# COMMAND ----------
+
+_tables_to_delete = _tables_to_delete_df.select("tableName").rdd.map(lambda r: r.tableName).collect()
+for t in _tables_to_delete:
+    spark.sql(f"DROP TABLE IF EXISTS {UC_CATALOG}.{UC_SCHEMA}.{t}")
+
+# COMMAND ----------
+
+
